2fc6dc5dae71e7e3a7889225e4ad1548b87f316b
kolnol <odnoshivkin@me.com>
2016-07-03 13:44:22 +0200

    Added structure to the page

--- js/init.js
+++ js/init.js
@@ -1,7 +1,8 @@
 (function($){
   $(function(){
-
-    $('.button-collapse').sideNav();
+      $(".button-collapse").sideNav();
+    $("#includedNavBar").load("html/navigation.html");
+    $("#includedFooter").load("html/footer.html");
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
