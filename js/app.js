6aa284d23eb2e7f32d2d47d0e712845d4e97ccec
kolnol <odnoshivkin@me.com>
2016-07-13 00:38:56 +0200

    Added AngularJS Logic

--- js/app.js
+++ js/app.js
@@ -14,15 +14,39 @@ var brmApp = angular.module("brmApp",['ui.materialize']);
 brmApp.controller("MainAppCtrl", function ($scope,$http) {
     $scope.data=model;
 
-    $scope.servUrl="http://85.214.195.89:8080/questions/getAll";
+    $scope.servUrl="http://85.214.195.89:8080";
     $scope.testUrl="questions.json";
 
-    $http.get($scope.testUrl).success(function (data, status, headers, config) {
+    $http.get($scope.servUrl+'/questions/getAll').success(function (data, status, headers, config) {
         if(data){
             $scope.data.questions=data;
             $scope.actualQuestion=$scope.data.questions[0];
         }
     });
+    $scope.testPostQuestion=[
+        {
+
+            content: "What the fuck is going on????",
+            possibleAnswers: ["Answer 1","Answer 2","Answer 3","Answer 4"],
+            rightAnswerIndex: 0,
+            thema: '',
+            kapitel: 1,
+            hint: '',
+            rightAnswered: false
+        }
+    ];
+
+
+    /*$http.post($scope.servUrl+'/questions/pushQuestions',$scope.testPostQuestion).then(function successCallback(response) {
+        // this callback will be called asynchronously
+        // when the response is available
+        console.log(response);
+    }, function errorCallback(response) {
+        // called asynchronously if an error occurs
+        // or server returns response with an error status.
+        console.log(response);
+    });*/
+
 
     $scope.goToQuestion=function (question) {
         $scope.actualQuestion=question;
@@ -30,6 +54,6 @@ brmApp.controller("MainAppCtrl", function ($scope,$http) {
 
     $scope.getRangeForChapters=function () {
         return new Array($scope.data.countOfChapters);
-    }
+    };
 
 });
