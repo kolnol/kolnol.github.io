2d25a8fbf55df890fb0f402d8bcc29cbee70b899
kolnol <odnoshivkin@me.com>
2016-08-13 02:58:47 +0200

    Added topic field to new question form Possibility to choose answer implemented

--- js/app.js
+++ js/app.js
@@ -55,9 +55,16 @@ brmApp.controller("MainAppCtrl", function ($scope,$http) {
         if(data){
             $scope.data.questions=data;
             $scope.actualQuestion=$scope.data.questions[0];
-            console.log($scope.actualQuestion.possibleAnswers[0].answer);
         }
     });
+
+    /*$http.get($scope.servUrl+'/questions/getAllTopics').success(function (data, status, headers, config) {
+        if(data){
+            $scope.data.topics=data;
+        }
+    });*///todo remove dummies for topics
+    $scope.data.topics = ['Topic 1','Topic 2','Topic 3','Topic 4','Topic 5','Topic 6'];
+
 
     $scope.data.user={name : "Peter Griffin",isAdmin : true,id : "9363bdobe"};//TODO Dummy
 
@@ -72,15 +79,49 @@ brmApp.controller("MainAppCtrl", function ($scope,$http) {
     });*/
 
 
-    $scope.testAlert=function(){
-      alert('hello');
+    $scope.testAlert=function(elem,answerId){
+      console.log(elem);
     };
+
+    $scope.isChoosingAnswerEnabled=true;
+
+    $scope.onAnswerClick=function (event,answerId) {
+        console.log(event);
+        var rightAnswerLetter;
+
+        var rightAnswerId=$scope.actualQuestion.correctAnswerId;
+        //TODO Make it better
+        switch (rightAnswerId){
+            case 0:
+                rightAnswerLetter='A';
+                break;
+            case 1:
+                rightAnswerLetter='B';
+                break;
+            case 2:
+                rightAnswerLetter='C';
+                break;
+            case 3:
+                rightAnswerLetter='D';
+                break;
+        };
+
+        var elem =angular.element(event.target);
+        $scope.isChoosingAnswerEnabled=false;
+        if(answerId===rightAnswerId){
+            elem.addClass('green lighten-3');
+            //Todo Implement send to server
+        }else{
+            elem.addClass('red lighten-3');
+            angular.element(document).find('#answer'+rightAnswerLetter).addClass('green lighten-3');
+        }
+    };
+
     $scope.goToQuestion=function (question) {
         $scope.actualQuestion=question;
     };
 
     $scope.getAllTopics=function () {//TODO Dummy
-        $scope.data.topics=["Start","Accounting","Year End","More Accounting","Weather"];
         return $scope.data.topics;
     };
 
@@ -95,6 +136,18 @@ brmApp.controller('addNewQuestionTabCtrl',function ($scope, $http) {
     $scope.newQuestion={};
     $scope.servUrl="http://85.214.195.89:8080/api";
 
+    /*$http.get($scope.servUrl+'/questions/getAllTopics').success(function (data, status, headers, config) {
+     if(data){
+     $scope.data.topics=data;
+     }
+     });*///todo remove dummies for topics
+
+    $scope.topics = ['Topic 1','Topic 2','Topic 3','Topic 4','Topic 5','Topic 6'];
+
+    $scope.getAllTopics=function () {//TODO delete dummy
+        return $scope.topics;
+    };
+
     $scope.onSubmit=function () {
         console.log($scope.newQuestion.content);
         console.log($scope.newQuestion.possibleAnswers);
@@ -106,12 +159,13 @@ brmApp.controller('addNewQuestionTabCtrl',function ($scope, $http) {
         console.log($scope.newQuestion.topic);
         console.log($scope.newQuestion.chapter);
 
-
     var questionsToSend=[$scope.newQuestion];
         questionsToSend[0].possibleAnswers=[{answerId:0 , answer:$scope.newQuestion.possibleAnswers[0]},
             {answerId:1 , answer:$scope.newQuestion.possibleAnswers[1]},
             {answerId:2 , answer:$scope.newQuestion.possibleAnswers[2]},
             {answerId:3 , answer:$scope.newQuestion.possibleAnswers[3]}];
+        questionsToSend.topic=$scope.data.topics[$scope.newQuestion.topic];
+        questionsToSend.chapter = $scope.newQuestion.topic+1;
         console.log(questionsToSend);
         $http.post($scope.servUrl+'/questions/pushQuestions',questionsToSend).then(function successCallback(response) {
             // this callback will be called asynchronously
@@ -165,23 +219,31 @@ brmApp.controller('PdfLecturesCtrl',function ($scope, $http) {
     };
 });
 
-brmApp.directive('myQuestion',['$http',function ($http) {
+brmApp.directive('myQuestion',['$http','$compile',function ($http,$compile) {
     var linkFn=function (scope, element, attrs, controller, transcludeFn) {
-      console.log(scope);
+      /*console.log(scope);
       console.log(element.contents());
       console.log(attrs);
       console.log(controller);
       console.log(transcludeFn);
-        console.log(scope.data.user);
+        console.log(scope.data.user);*/
 
         var isQuestionChanged = false;
 
+
         var setNormalQuestionView=function () {
-            element.find("#content").replaceWith('<h5 class="center-align" id="content">'+scope.actualQuestion.content+'</h5>');
-            element.find("#answerA").replaceWith('<a href="#!" class="collection-item" id="answerA">'+scope.actualQuestion.possibleAnswers[0].answer+'</a>');
-            element.find("#answerB").replaceWith('<a href="#!" class="collection-item" id="answerB">'+scope.actualQuestion.possibleAnswers[1].answer+'</a>');
-            element.find("#answerC").replaceWith('<a href="#!" class="collection-item" id="answerC">'+scope.actualQuestion.possibleAnswers[2].answer+'</a>');
-            element.find("#answerD").replaceWith('<a href="#!" class="collection-item" id="answerD">'+scope.actualQuestion.possibleAnswers[3].answer+'</a>');
+            var contentTemplate = $compile('<h5 class="center-align" id="content">'+scope.actualQuestion.content+'</h5>')(scope);
+            var answerATemplate = $compile('<a ng-click="!isChoosingAnswerEnabled||onAnswerClick($event,0)" class="collection-item" id="answerA">'+scope.actualQuestion.possibleAnswers[0].answer+'</a>')(scope);
+            var answerBTemplate = $compile('<a ng-click="!isChoosingAnswerEnabled||onAnswerClick($event,1)" class="collection-item" id="answerB">'+scope.actualQuestion.possibleAnswers[1].answer+'</a>')(scope);
+            var answerCTemplate = $compile('<a ng-click="!isChoosingAnswerEnabled||onAnswerClick($event,2)" class="collection-item" id="answerC">'+scope.actualQuestion.possibleAnswers[2].answer+'</a>')(scope);
+            var answerDTemplate = $compile('<a ng-click="!isChoosingAnswerEnabled||onAnswerClick($event,3)" class="collection-item" id="answerD">'+scope.actualQuestion.possibleAnswers[3].answer+'</a>')(scope);
+
+
+            element.find("#content").replaceWith(contentTemplate);
+            element.find("#answerA").replaceWith(answerATemplate);
+            element.find("#answerB").replaceWith(answerBTemplate);
+            element.find("#answerC").replaceWith(answerCTemplate);
+            element.find("#answerD").replaceWith(answerDTemplate);
 
             if(scope.data.user.isAdmin){
                 element.find("#editButton").text('Edit');
@@ -210,17 +272,26 @@ brmApp.directive('myQuestion',['$http',function ($http) {
 
       });
 
+
+
+
+
         var onEditClick= function (editButton) {
 
             isQuestionChanged=true;
 
             editButton.text('Save');
+            var contentTemplate = $compile('<input placeholder="Question" id="content" type="text" value="'+scope.actualQuestion.content+'" class="validate">')(scope);
+            var answerATemplate = $compile('<input placeholder="Answer A" id="answerA" type="text" value="'+scope.actualQuestion.possibleAnswers[0].answer+'" class="validate">')(scope);
+            var answerBTemplate = $compile('<input placeholder="Answer B" id="answerB" type="text" value="'+scope.actualQuestion.possibleAnswers[1].answer+'" class="validate">')(scope);
+            var answerCTemplate = $compile('<input placeholder="Answer C" id="answerC" type="text" value="'+scope.actualQuestion.possibleAnswers[2].answer+'" class="validate">')(scope);
+            var answerDTemplate = $compile('<input placeholder="Answer D" id="answerD" type="text" value="'+scope.actualQuestion.possibleAnswers[3].answer+'" class="validate">')(scope);
 
-            element.find("#content").replaceWith('<input placeholder="Placeholder" id="content" type="text" value="'+scope.actualQuestion.content+'" class="validate">');
-            element.find("#answerA").replaceWith('<input placeholder="Placeholder" id="answerA" type="text" value="'+scope.actualQuestion.possibleAnswers[0].answer+'" class="validate">');
-            element.find("#answerB").replaceWith('<input placeholder="Placeholder" id="answerB" type="text" value="'+scope.actualQuestion.possibleAnswers[1].answer+'" class="validate">');
-            element.find("#answerC").replaceWith('<input placeholder="Placeholder" id="answerC" type="text" value="'+scope.actualQuestion.possibleAnswers[2].answer+'" class="validate">');
-            element.find("#answerD").replaceWith('<input placeholder="Placeholder" id="answerD" type="text" value="'+scope.actualQuestion.possibleAnswers[3].answer+'" class="validate">');
+            element.find("#content").replaceWith(contentTemplate);
+            element.find("#answerA").replaceWith(answerATemplate);
+            element.find("#answerB").replaceWith(answerBTemplate);
+            element.find("#answerC").replaceWith(answerCTemplate);
+            element.find("#answerD").replaceWith(answerDTemplate);
 
         };
 
@@ -273,7 +344,7 @@ brmApp.directive('myQuestion',['$http',function ($http) {
         restrict : 'E',
         scope : true,
         templateUrl: 'html/mainContentMC.html',
-        require:"^?ngShow",
+        require:["^?ngShow","^?ngClick"],
         link:linkFn
     }
 }]);
